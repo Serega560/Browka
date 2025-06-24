fb49d3030c0672fce1ae08857fd012e8f97ac6f2
Serega560 <116281028+Serega560@users.noreply.github.com>
2025-06-24 21:56:41 +0300

    Add files via upload

---
+++ src/assets/components/Footer.vue
@@ -0,0 +1,54 @@
+<script setup>
+import Logo from '@/components/Logo.vue'
+import Social from '@/components/Social.vue'
+import Menu from '@/components/Menu.vue'
+</script>
+
+<template>
+<div class="footer">
+  <div class="container">
+    <h2 class="footer__title">Контакты</h2>
+    <div class="footer__block">
+
+      <Logo />
+      <Menu :inFooter="true" />
+      <Social />
+
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/styles/media.scss' as *;
+@use '@/assets/styles/container' as *;
+
+.footer {
+  background-color: var(--color-default-black);
+  padding: 120px 0;
+
+  @include vp-767 {
+    padding: 40px 0;
+  }
+
+  .footer__title {
+    color: var(--color-default-white);
+    font-size: 45px;
+    margin-bottom: 70px;
+    text-align: center;
+    text-transform: uppercase;
+
+    @include vp-767 {
+      margin-bottom: 25px;
+      font-size: 28px;
+    }
+  }
+
+  .footer__block {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
+
+</style>
